93fcf8eef24b5d5993267e71f98e2f124c77da03
enockomondi <djseanizellkenya@gmail.com>
2024-03-18 12:49:34 +0300

    updated website colors to match company brand colors

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/novustell/Desktop/APPS/JDAFINAL/JDA-API/env/bin/python3
+#!/home/novustell/Desktop/APPS/Novustellke/env/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
