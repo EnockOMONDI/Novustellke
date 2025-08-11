c48d16a6e95b91b1f9ef292b67ec33918dcac6af
Enock <djsean@Enocks-MBP.lan>
2025-08-11 12:09:11 +0300

    wow

--- tours_travels/urls.py
+++ tours_travels/urls.py
@@ -36,6 +36,8 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/index.html'),name='logout'),
     path('mail/',tours_travels_views.mail,name='mail'),
+    path('status/', include(('status.urls', 'status'), namespace='status')),
+    path('system-status/', include(('status.urls', 'status'), namespace='system-status')),
 
     path('', include('users.urls')),
 
